0c404234d3a2e764976e29c6fe37f67a5fea497b
maximilianocup <63514082+maximilianocup@users.noreply.github.com>
2020-10-03 13:39:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const obtenerCerveza = require("./library");
 obtenerCerveza(2139068).then(data => {
     //Data.forEach(cerveza => {
         console.log("Nombre: " + data.name);
+        console.log("Imagen: " + data.image_url);
         console.log("Precio: " + data.price);
         console.log("Categoria: " + data.category);
         console.log("estilo: " + data.style);
